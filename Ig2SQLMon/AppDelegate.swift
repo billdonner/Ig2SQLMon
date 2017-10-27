4ae0a82346ee3b26c15a369baeae6e0efb889a92
Bill Donner <billdonner@gmail.com>
2017-10-27 12:42:05 -0400

    Initial Commit

---
+++ Ig2SQLMon/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Ig2SQLMon
+//
+//  Created by william donner on 10/27/17.
+//  Copyright © 2017 midnightrambler. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Ig2SQLMon/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Ig2SQLMon
+//
+//  Created by william donner on 10/27/17.
+//  Copyright © 2017 midnightrambler. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
